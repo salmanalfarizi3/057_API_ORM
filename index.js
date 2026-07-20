2b39ca876721cc154b1542a806aff50b2dee0d68
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-07-20 15:32:04 +0700

    feat(komik): tambahkan validasi pengecekan keberadaan komik dan respon 404

--- index.js
+++ index.js
@@ -58,4 +58,10 @@ app.put('/komik/:id', async (req, res) => {
 
 app.delete('/komik/:id', async (req, res) => {
     const id = req.params.id;
-    
+    try {
+        const komik = await db.Komik.findByPk(id);
+        if (!komik) {
+            return res.status(404).send({ message: 'Komik not found' });
+        }
+
+       
