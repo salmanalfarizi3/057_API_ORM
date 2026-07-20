92c069c5bda7d37bb8dee45c986a2fa2cbb8fe1a
salmanalfarizi3 <salmanalfariziiel@gmail.com>
2026-07-20 15:07:37 +0700

    feat(server): hubungkan sinkronisasi database Sequelize dan jalankan server Express

--- index.js
+++ index.js
@@ -7,4 +7,13 @@ app.use(express.urlencoded({
     extended: false
  }));
 
- 
+ app.listen(port, () => {
+    console.log(`Server started on port 3000`);
+ })
+
+ db.sequelize.sync()
+ .then((result) => {
+    app.listen(3000, () => {
+        console.log('Server started');
+    })
+})
